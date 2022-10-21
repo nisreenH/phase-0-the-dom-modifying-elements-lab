3174914b811e3074a0d7695dcde84feb31eedb29
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-10-21 15:31:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement("H1");
+
+
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Nisreen is the champion";
+
+document.body.append(newHeader);
